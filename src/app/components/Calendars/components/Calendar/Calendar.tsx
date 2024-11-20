3fb30e376d98e6472b68fd45303af4c15d04b94d
p-cholocinski <piotr.cholocinski@gmail.com>
2024-11-20 21:07:24 +0100

    comment reservationObj

--- src/app/components/Calendars/components/Calendar/Calendar.tsx
+++ src/app/components/Calendars/components/Calendar/Calendar.tsx
@@ -7,24 +7,24 @@ import getCalendarDays from "@/lib/getCalendarDays";
 
 export default function Calendar() {
 
-  const reservationsObj: Reservation[] = [
-    {
-      dateFrom: new Date('20.11.2024 09:00'),
-      dateTo: new Date('20.11.2024 10:00'),
-      category: 'spotkanie',
-      description: 'Plan Kontrli Jakości Systemów - Pobieranie oprogramowania',
-      roomId: 'room-1',
-      userId: 'user-1',
-    },
-    {
-      dateFrom: new Date('20.11.2024 11:00'),
-      dateTo: new Date('20.11.2024 12:00'),
-      category: 'spotkanie',
-      description: 'Arrow K',
-      roomId: 'room-1',
-      userId: 'user-1',
-    }
-  ]
+  /*   const reservationsObj: Reservation[] = [
+      {
+        dateFrom: new Date('20.11.2024 09:00'),
+        dateTo: new Date('20.11.2024 10:00'),
+        category: 'spotkanie',
+        description: 'Plan Kontrli Jakości Systemów - Pobieranie oprogramowania',
+        roomId: 'room-1',
+        userId: 'user-1',
+      },
+      {
+        dateFrom: new Date('20.11.2024 11:00'),
+        dateTo: new Date('20.11.2024 12:00'),
+        category: 'spotkanie',
+        description: 'Arrow K',
+        roomId: 'room-1',
+        userId: 'user-1',
+      }
+    ] */
 
   const { calendarPeriod } = useCalendarPeriodContext()
   const { calendarType } = useCalendarTypeContext()
